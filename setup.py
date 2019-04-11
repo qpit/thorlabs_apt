679de31b18f3d3b8ceed03380e3e8d7adc97fe4c
Tobias Gehring <main@tobiasgehring.de>
2019-04-11 21:40:49 +0200

    updated setup.py to work with current python versions

--- setup.py
+++ setup.py
@@ -9,16 +9,16 @@ DESCRIPTION = 'python wrapper for Thorlabs\' APT library'
 LONG_DESCRIPTION = '''\
 thorlabs_apt provides a wrapper for Thorlabs\' APT, a library to control different Thorlabs motors.
 '''
-CLASSIFIERS = """\
-Development Status :: 4 - Beta
-Intended Audience :: Science/Research
-License :: OSI Approved :: GNU General Public License v2 (GPLv2)
-Programming Language :: Python
-Topic :: Scientific/Engineering
-Topic :: Software Development :: Libraries
-Topic :: System :: Hardware
-Operating System :: Microsoft :: Windows
-"""
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development :: Libraries'
+    'Topic :: System :: Hardware'
+    'Operating System :: Microsoft :: Windows'
+]
 PLATFORMS = ['Windows']
 MAJOR               = 0
 MINOR               = 1
@@ -39,6 +39,6 @@ if __name__=='__main__':
         version = VERSION,
         description = DESCRIPTION,
         long_description = LONG_DESCRIPTION,
-        classifiers = filter(None, CLASSIFIERS.split('\n')),
+        classifiers = CLASSIFIERS,
         platforms = PLATFORMS,
         packages = ['thorlabs_apt'])
